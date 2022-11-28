b42020f6f71083df7a4101a59a6556e27419f6e0
Hayden50 <haydenrieder50@hotmail.com>
2022-11-28 16:25:30 -0500

    Add Axios to backend (API REQ BUGGED)

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,23 @@
 import express from "express";
+import axios from "axios";
 
 const router = express.Router();
+const options = {
+  method: "GET",
+  url: "https://api-nba-v1.p.rapidapi.com/players/statistics",
+  params: { id: "236", season: "2020" },
+  headers: {
+    "X-RapidAPI-Key": "b637575e6bmshbab4c4bc6a23a7bp10b843jsn126610645d1b",
+    "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+  },
+};
 
 router.get("/", (req, res) => {
-  res.send("Test Page");
+  axios
+    .request(options)
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+  res.send("loading...");
 });
 
 export default router;

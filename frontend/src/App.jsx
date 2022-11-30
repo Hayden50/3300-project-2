7d07d06548490e12822ec643b8d51fef03229669
Hayden50 <haydenrieder50@hotmail.com>
2022-11-29 19:00:29 -0500

    Add border outlines

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,16 @@
-import axios from "axios"
-import { useState } from "react";
-import Person from './components/Person'
 import NameForm from './components/NameForm'
 import "./App.css"
  
-
-
-
 const App = () => {
-
-  const [val, setVal] = useState('');
-  //axios.get('/api/').then(res => setVal(res.data)).then(res => console.log(res));
   return (
-    <div className = "App">
-      <h1>NBA Player Comparison</h1>
-      <NameForm/>
+    <div className = "parent">
+      {/* <h1>NBA Player Comparison</h1>
+      <NameForm/> */}
+      <div className='div1'>1</div>
+      <div className='div2'>2</div>
+      <div className='div3'>3</div>
+      <div className='div4'>4</div>
+      <div className='div5'>5</div>
     </div>
   )
 }
